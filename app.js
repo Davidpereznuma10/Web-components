69614d63c51b6e837705630caa90fc2f6c7c6a02
Davidpereznuma10 <Jdpereznuma@gmail.com>
2023-05-04 06:36:51 -0500

    feat: :sparkles: Add web component my-nave
    add the web-component my-nave since the fiñle components like module

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import '/components/myheader/my-header.js';
 import '/components/mysection/my-section.js'
 import '/components/myselection/my-selection.js'
+import '/components/myNave/my-nave.js'
 console.log('hola');
---
+++ components/myNave/my-nave.js
@@ -0,0 +1,15 @@
+let pathName = new URL(import.meta.url).pathname;
+let name = pathName.split('/').pop().replace('.js','');
+
+export default class myNave extends HTMLElement{
+    static async component(){
+        return await(await fetch(pathName.replace('.js','.html'))).text();}
+        constructor(){
+            super();
+            this.attachShadow({mode:'open'});
+            Promise.resolve(myNave.component()).then(html=>{
+                this.shadowRoot.innerHTML=html;
+            })
+        }
+}
+customElements.define(name, myNave)
