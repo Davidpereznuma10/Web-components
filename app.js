34deceb012311f22b1eb6d47279244b1522d32c0
Davidpereznuma10 <Jdpereznuma@gmail.com>
2023-05-04 07:06:22 -0500

    fix: :bug: secodn attempt
    try to fix the bug

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-import '/components/myheader/my-header.js';
-import '/components/mysection/my-section.js'
-import '/components/myselection/my-selection.js'
-import '/components/myNave/my-nave.js'
-import '/components/myfooter/my-footer.js'
+import './components/myheader/my-header.js';
+import './components/mysection/my-section.js'
+import './components/myselection/my-selection.js'
+import './components/myNave/my-nave.js'
+import './components/myfooter/my-footer.js'
