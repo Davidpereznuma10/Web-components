4ac20995c0aa576e0390c1c68bb2b6eceedda624
Davidpereznuma10 <Jdpereznuma@gmail.com>
2023-05-04 08:20:50 -0500

    feat:

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-import 'components/myheader/my-header.js';
-import 'components/mysection/my-section.js'
-import 'components/myselection/my-selection.js'
-import 'components/myNave/my-nave.js'
-import 'components/myfooter/my-footer.js'
+import  myHeader from './components/myheader/my-header.js';
+import mysection  from './components/mysection/my-section.js'
+import myselection  from './components/myselection/my-selection.js'
+import  myNave from './components/myNave/my-nave.js'
+import  myfooter from './components/myfooter/my-footer.js'
