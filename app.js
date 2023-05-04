7720b97a059b41d0e2bddb6ee1dc14703746e17a
Davidpereznuma10 <Jdpereznuma@gmail.com>
2023-05-04 06:41:35 -0500

    feat: :sparkles: add the web-component my-footer
    the component is a module that is insert from app.js, all the styles are into the html

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ import '/components/myheader/my-header.js';
 import '/components/mysection/my-section.js'
 import '/components/myselection/my-selection.js'
 import '/components/myNave/my-nave.js'
-console.log('hola');
+import '/components/myfooter/my-footer.js'
